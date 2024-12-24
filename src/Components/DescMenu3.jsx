ad9cd30e68f2f0ad176db981c79374d97b181c4d
mokarram280 <mdmokarramhossain98@gmail.com>
2024-12-24 13:25:51 +0600

    step5 section done

--- src/Components/DescMenu3.jsx
+++ src/Components/DescMenu3.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { GrClose } from "react-icons/gr";
 
-export const DescMenu2 = () => {
+export const DescMenu3 = () => {
   return (
     <section className='bg-[#E4E4E780] px-8 lg:px-12 xl:px-[86px] pt-8 pb-[29px]'>
         <div className='flex flex-col md:flex-row items-center justify-between gap-4'>
@@ -17,14 +17,14 @@ export const DescMenu2 = () => {
         <div className='ms-10 hidden xl:flex'><img src="/hline.svg" alt="Line" className='w-32 xl:w-full' /></div>
       </div>
       <div className='flex flex-col md:flex-row items-center justify-center gap-[14px] text-[12px] font-normal'>
-        <Link to='/describes'>
+        <Link to='/step4'>
         <button className='bg-white text-black py-4 px-[55px] rounded-md border border-gray-200 font-bold'>
           Back
         </button>
         </Link>
-        <Link to='/step5'>
+        <Link to='/dashboard'>
         <button className='bg-primary text-white py-4 px-[25px] rounded-md font-bold'>
-           Next step
+           Finished
         </button>
         </Link>
       </div>
